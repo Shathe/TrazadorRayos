a29d697889b0762aa68ce6fd103be7f0f4f23183
Alejandro Dieste Cortes <alej.14@hotmail.com>
2015-11-22 10:27:12 +0100

    a sin modular, con antialiasing

--- src/trazadorrayos/OperacionesEscena.java
+++ src/trazadorrayos/OperacionesEscena.java
@@ -27,15 +27,18 @@ public class OperacionesEscena {
 						distanciaMenor = distancia;
 						figura = siguienteFigura;
 					}
+
 				}
 			}
 		}
+
 		return figura;
 	}
 
 	public static Color colorPuntoPantalla(Point4d puntoPixelPantalla,
 			Escena escena, int maxDepth, double minIntensity) {
 		Color color = new Color(0, 0, 0);
+
 		/*
 		 * Ahora se crea un rayo que pase por el punto de la pantalla y con
 		 * direccion (puntoCamara-puntopantalla)
@@ -54,7 +57,9 @@ public class OperacionesEscena {
 		 */
 		color = colorDesdeRayo(escena, rayo, maxDepth, minIntensity, null,
 				false);
+
 		return color;
+
 	}
 
 	/*
@@ -62,7 +67,9 @@ public class OperacionesEscena {
 	 */
 	public static Color colorDesdeRayo(Escena escena, Rayo rayo, int MaxDepth,
 			double minIntensity, Figura desde, boolean refractadoParaSalir) {
+
 		Color color = null;
+
 		Figura figura = OperacionesEscena.FiguraMasCercana(escena.getFiguras(),
 				rayo.getPunto(), rayo, escena.getCamara(), desde);
 		if (figura != null && Interseccion.intersecta(rayo, figura) != null) {
@@ -71,20 +78,56 @@ public class OperacionesEscena {
 			 * su color
 			 */
 			Point4d punto = Interseccion.intersecta(rayo, figura);
+
 			double reflec = figura.getIndiceReflectividad();
 			double refrac = 0;
 			if (figura.getIndiceRefraccion() != 0)
 				refrac = 1 / figura.getIndiceRefraccion();// suponemos que el
 															// exterior es 1
-			Vector4d normal = figura.getNormal(punto);
-			normal.normalize();
-			rayo.getDireccion().normalize();
+
 			/*
 			 * Ahora se calcula la direccion del angulo refractado y del angulo
 			 * reflejado
 			 */
-			Vector4d reflejado = rayoReflejado(normal, rayo);
-			Vector4d refractado = rayoRefractado(refrac, normal, rayo);
+
+			Vector4d refractado = new Vector4d();
+
+			Vector4d normal = figura.getNormal(punto);
+
+			// reflejado => V-2*(V*N)N
+
+			// aux=2*(V*N)
+			normal.normalize();
+			rayo.getDireccion().normalize();
+			double aux = 2 * normal.dot(rayo.getDireccion());
+			Vector4d reflejado = new Vector4d();
+			// 2*(V*N)N
+			reflejado.x = aux * normal.x;
+			reflejado.y = aux * normal.y;
+			reflejado.z = aux * normal.z;
+			reflejado.w = normal.w;
+			// V-2*(V*N)N
+			reflejado.x = rayo.getDireccion().x - reflejado.x;
+			reflejado.z = rayo.getDireccion().z - reflejado.z;
+			reflejado.y = rayo.getDireccion().y - reflejado.y;
+			reflejado.w = rayo.getDireccion().w - reflejado.w;
+			reflejado.normalize();
+			// ahora calculas el angulo reflejado
+			// T=(Iref*(normal*Rayo)-Raiz(1-Iref²(1-(normal*rayo)²)))
+			// *normal-Iref*rayo
+			Vector4d direccionRayo = new Vector4d(rayo.getDireccion());
+			direccionRayo.negate();
+
+			double auxR = (refrac * (normal.dot(direccionRayo)))
+					- Math.sqrt(1 - (refrac * refrac * (1 - (normal
+							.dot(direccionRayo) * normal.dot(direccionRayo)))));
+
+			refractado.x = auxR * normal.x - refrac * direccionRayo.x;
+			refractado.y = auxR * normal.y - refrac * direccionRayo.y;
+			refractado.z = auxR * normal.z - refrac * direccionRayo.z;
+			refractado.w = 0;
+			refractado.normalize();
+
 			/*
 			 * Ahora tienes el angulo refractado y el angulo reflejado, ahora
 			 * para calcular el color, primero tienes que mirar el color que
@@ -102,6 +145,7 @@ public class OperacionesEscena {
 
 			double noVisible = interseccionAFocoTapado(punto, escena.getFoco(),
 					escena.getFiguras(), figura);
+
 			if (refractadoParaSalir) {
 				double intensidadRefractadoR = (1 - reflec)
 						* escena.getFoco().getColor().getRed();
@@ -126,17 +170,23 @@ public class OperacionesEscena {
 					int blue = (int) (intensidad * figura.kd.getBlue());
 					int green = (int) (intensidad * figura.kd.getGreen());
 					color = new Color(red, green, blue);
+
 				}
 				else {
 					double TapanObjetosTranslucidos = 1 - noVisible;
 					// Calculas la intensidad en tu punto
 					Vector4d rayoAlOjo = new Vector4d();
-					rayoAlOjo.sub(escena.getCamara().getPosicion(), punto);
-					rayoAlOjo.normalize();
+					rayoAlOjo.x = escena.getCamara().getPosicion().x - punto.x;
+					rayoAlOjo.y = escena.getCamara().getPosicion().y - punto.y;
+					rayoAlOjo.z = escena.getCamara().getPosicion().z - punto.z;
+					rayoAlOjo.w = 0;
 					Vector4d rayoAlFoco = new Vector4d();
-					rayoAlFoco.sub(escena.getFoco().getPosicion(), punto);
+					rayoAlFoco.x = escena.getFoco().getPosicion().x - punto.x;
+					rayoAlFoco.y = escena.getFoco().getPosicion().y - punto.y;
+					rayoAlFoco.z = escena.getFoco().getPosicion().z - punto.z;
+					rayoAlFoco.w = 0;
 					rayoAlFoco.normalize();
-					
+					rayoAlOjo.normalize();
 					// ambiental
 					double intensidadR = escena.getFoco()
 							.getIntensidadAmbiente() * figura.kd.getRed();
@@ -162,17 +212,26 @@ public class OperacionesEscena {
 					// especular
 					rayoAlOjo.normalize();
 					double aux2 = 2 * normal.dot(rayoAlFoco);
+
 					Vector4d reflejado2 = new Vector4d();
-					// 2*(L*N)N
-					reflejado2.scale(aux2);
-					// 2*(L*N)N - L
-					reflejado2.sub(rayoAlFoco, reflejado2);
+					// 2*(l*N)N
+					reflejado2.x = aux2 * normal.x;
+					reflejado2.y = aux2 * normal.y;
+					reflejado2.z = aux2 * normal.z;
+					reflejado2.w = normal.w;
+
+					// 2*(V*N)N - l
+					reflejado2.x = rayoAlFoco.x - reflejado2.x;
+					reflejado2.z = rayoAlFoco.z - reflejado2.z;
+					reflejado2.y = rayoAlFoco.y - reflejado2.y;
+					reflejado2.w = rayoAlFoco.w - reflejado2.w;
 					reflejado2.normalize();
 					reflejado2.negate();
-					//cos (Ref, View)
 					double cosenoEsp = reflejado2.dot(rayoAlOjo)
 							/ rayoAlOjo.length() / reflejado2.length();
+
 					if (cosenoEsp < 0) cosenoEsp = 0;
+
 					cosenoEsp = Math.pow(cosenoEsp, 150);
 					double IpartEspR = figura.ks.getRed()
 							* escena.getFoco().getColor().getRed() / 255
@@ -183,12 +242,14 @@ public class OperacionesEscena {
 					double IpartEspB = figura.ks.getBlue()
 							* escena.getFoco().getColor().getBlue() / 255
 							* cosenoEsp * TapanObjetosTranslucidos;
+
 					intensidadR += IpartDifR + IpartEspR;
 					intensidadG += IpartDifG + IpartEspG;
 					intensidadB += IpartDifB + IpartEspB;
 					int red = (int) (intensidadR);
 					int green = (int) (intensidadG);
 					int blue = (int) (intensidadB);
+
 					int intensidadReflejadaR = (int) (reflec * IpartDifR);
 					int intensidadReflejadaG = (int) (reflec * IpartDifG);
 					int intensidadReflejadaB = (int) (reflec * IpartDifB);
@@ -196,6 +257,7 @@ public class OperacionesEscena {
 							intensidadReflejadaG, intensidadReflejadaB);
 					Rayo rayoReflejado = new Rayo(reflejado, punto,
 							colorRayoReflejado);
+
 					double intensidadRefractadoR = (1 - reflec)
 							* escena.getFoco().getColor().getRed();
 					double intensidadRefractadoG = (1 - reflec)
@@ -208,6 +270,7 @@ public class OperacionesEscena {
 							(int) intensidadRefractadoB);
 					Rayo rayoRefractado = new Rayo(refractado, punto,
 							colorRayoRefractado);
+
 					if (MaxDepth > 0
 							&& (intensidadReflejadaR + intensidadReflejadaG + intensidadReflejadaB) > minIntensity) {
 						Color colorReflejado = colorDesdeRayo(escena,
@@ -217,6 +280,7 @@ public class OperacionesEscena {
 						blue += colorReflejado.getBlue();
 						green += colorReflejado.getGreen();
 					}
+
 					if (MaxDepth > 0
 							&& (intensidadRefractadoR + intensidadRefractadoB + intensidadRefractadoG) > minIntensity) {
 						boolean esperarRefraccion = false;
@@ -229,9 +293,13 @@ public class OperacionesEscena {
 						red += colorRefractado.getRed();
 						blue += colorRefractado.getBlue();
 						green += colorRefractado.getGreen();
+
 					}
+
 					color = normalizarColor(red, green, blue);
+
 				}
+
 			}
 		}
 		else {
@@ -242,11 +310,13 @@ public class OperacionesEscena {
 	}
 
 	public static Color normalizarColor(int red, int green, int blue) {
+
 		int mayor = 0;
 		if (blue > mayor) mayor = blue;
 		if (red > mayor) mayor = red;
 		if (green > mayor) mayor = green;
 		if (mayor > 255) {
+
 			double indiceReduccion = (double) mayor / 255;
 			indiceReduccion = Math.sqrt(indiceReduccion);
 			red = (int) (red / indiceReduccion);
@@ -260,56 +330,6 @@ public class OperacionesEscena {
 		// System.out.println(red+" "+green+" "+blue);
 		return new Color(red, green, blue);
 
-	}
-
-	/**
-	 * Devuelve el rayo reflejado del rayo incidente
-	 * 
-	 * @param normal
-	 * @param rayo
-	 * @return
-	 */
-	public static Vector4d rayoReflejado(Vector4d normal, Rayo rayo) {
-		double aux = 2 * normal.dot(rayo.getDireccion());
-		Vector4d reflejado = new Vector4d();
-		// 2*(V*N)N
-		reflejado.x = aux * normal.x;
-		reflejado.y = aux * normal.y;
-		reflejado.z = aux * normal.z;
-		reflejado.w = normal.w;
-		// V-2*(V*N)N
-		reflejado.x = rayo.getDireccion().x - reflejado.x;
-		reflejado.z = rayo.getDireccion().z - reflejado.z;
-		reflejado.y = rayo.getDireccion().y - reflejado.y;
-		reflejado.w = rayo.getDireccion().w - reflejado.w;
-		reflejado.normalize();
-		return reflejado;
-	}
-
-	/**
-	 * Devuelve el rayo refractado del rayo incidente
-	 * 
-	 * @param refrac
-	 * @param normal
-	 * @param rayo
-	 * @return
-	 */
-	public static Vector4d rayoRefractado(double refrac, Vector4d normal,
-			Rayo rayo) {
-		Vector4d refractado = new Vector4d();
-		Vector4d direccionRayo = new Vector4d(rayo.getDireccion());
-		direccionRayo.negate();
-
-		double auxR = (refrac * (normal.dot(direccionRayo)))
-				- Math.sqrt(1 - (refrac * refrac * (1 - (normal
-						.dot(direccionRayo) * normal.dot(direccionRayo)))));
-
-		refractado.x = auxR * normal.x - refrac * direccionRayo.x;
-		refractado.y = auxR * normal.y - refrac * direccionRayo.y;
-		refractado.z = auxR * normal.z - refrac * direccionRayo.z;
-		refractado.w = 0;
-		refractado.normalize();
-		return refractado;
 	}
 
 	/**
